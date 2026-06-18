d62079c2006f872704d818e2d3d2c7111ded9ca1
luis cordoba <lucordobaga@est.utn.ac.cr>
2026-06-18 16:45:37 -0600

    se concreto la clase arbitro
    se le pusieron los metodos get como set y el constructor

--- src/arbitros/Arbitro.java
+++ src/arbitros/Arbitro.java
@@ -9,5 +9,20 @@ package arbitros;
  * @author Student
  */
 public class Arbitro {
-    
+        private int categoria;
+        
+    public int getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+    public Arbitro(int categoria) {
+        this.categoria = categoria;
+    }
+   @Override
+    public String toString(){
+    public String categoria1 = categoria==1? "Principal":"Asistente";
+    return "\n Arbitro: "+ categoria1+
+    }
 }
