99162bfa4561b0a25ea32949b152a29f42675676
Aaron2312-dq <aadiazqu@est.utn.ac.cr>
2026-06-18 16:10:43 -0600

    Se refactorizo la clase entrenador
    Se crearon los respectivos atributos, con los metodos de acceso y to String. Esta clase hereda los atributos de la clase persona

--- src/entrenadores/Entrenador.java
+++ src/entrenadores/Entrenador.java
@@ -3,11 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package entrenadores;
+import java.time.LocalDate;
+import personas.Persona;
 
 /**
  *
  * @author Student
  */
-public class Entrenador {
+public class Entrenador extends Persona {
+    
+    private int añosExperiencia;
+    
+    public Entrenador(String nombre, String nacionalidad, LocalDate fechaNacimiento) {
+        super(nombre, nacionalidad, fechaNacimiento);
+    }
+    
+    public int getAñosExperiencia() {
+        return añosExperiencia;
+    }
+    public void setAñosExperiencia(int añosExperiencia) {
+        this.añosExperiencia = añosExperiencia;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "\nNombre"+getNombre() + "\nNacionalidad"+getNacionalidad() +
+                "\nFechaNacimiento"+getFechaNacimiento()+"\nAñosExperiencia=" + añosExperiencia;
+    }
+    
     
 }
