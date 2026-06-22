4469a83318028f91a792ec2b99d2e938db6f5b5e
estarli-var <lucordobaga@est.utn.ac.cr>
2026-06-22 15:37:54 -0600

    se hizo la clase mundial y casi esta terminada
    solo le falta terminar agregar el nombre de las 2 seleciones que van a jugar del atributo partidos

--- src/mundiales/Mundial.java
+++ src/mundiales/Mundial.java
@@ -4,10 +4,59 @@
  */
 package mundiales;
 
+import java.time.LocalDate;
+import selecciones.Seleccion;
 /**
  *
  * @author Student
  */
 public class Mundial {
+   private String nombre;
+   private LocalDate año;
+   private String pais; 
+   private String seleccion; 
+   private String partidos; 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getAño() {
+        return año;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public String getSeleccion() {
+        return seleccion;
+    }
+
+    public String getPartidos() {
+        return partidos;
+    }
+
+    public void setAño(LocalDate año) {
+        this.año = año;
+    }
+
+    public Mundial(String nombre, LocalDate año, String pais, Seleccion Nseleccion, String partidos) {
+        this.nombre = nombre;
+        this.año = año;
+        this.pais = pais;
+        this.seleccion = Nseleccion.getNombre();
+        this.partidos = partidos;
+    }
+
     
+
+    @Override
+    public String toString() {
+        return "Mundial{" + "nombre=" + nombre + ", año=" + año + ", pais=" + pais + ", seleccion=" + seleccion+ ", partidos=" + partidos + '}';
+    }
+    
+    
+   
+   
 }
