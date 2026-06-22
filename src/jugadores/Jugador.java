375817f908eea6e0600599307948ce61f39b0787
Elí Alvarado <elalvaradoob@est.utn.ac.cr>
2026-06-22 15:37:19 -0600

    Se actualiza jugador

--- src/jugadores/Jugador.java
+++ src/jugadores/Jugador.java
@@ -3,12 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package jugadores;
-
+import java.time.LocalDate;
+import personas.Persona;
 /**
  *
  * @author emalv
  */
-public class Jugador {
+public class Jugador extends Persona{
     
+    private int numero;
+    private String posicion;
+
+    public int getNumero() {
+        return numero;
+    }
+    
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public Jugador(int numero, String posicion, String nombre, String nacionalidad, LocalDate fechaNacimiento) {
+        super(nombre, nacionalidad, fechaNacimiento);
+        this.numero = numero;
+        this.posicion = posicion;
+    }
     
 }
