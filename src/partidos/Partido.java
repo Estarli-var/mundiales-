ffe822941b9eb05d3d380f37d62c1986682a6329
Wesley Ruiz <wruizm@est.utn.ac.cr>
2026-06-22 15:50:23 -0600

    Se agrega la clase partido
    Se agrega los metodos de acceso, contructor

--- src/partidos/Partido.java
+++ src/partidos/Partido.java
@@ -4,10 +4,69 @@
  */
 package partidos;
 
+import arbitros.Arbitro;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import resultados.Resultado;
+import selecciones.Seleccion;
+
 /**
  *
  * @author Student
  */
 public class Partido {
+    private Seleccion seleccionLocal;
+    private Seleccion seleccionVisitante;
+    private String estadio;
+    private LocalDate fecha;
+    private LocalTime hora;
+    protected Arbitro arbitro[];
+    private Resultado resultado;
+
+    public Seleccion getSeleccionLocal() {
+        return seleccionLocal;
+    }
+
+    public Seleccion getSeleccionVisitante() {
+        return seleccionVisitante;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public Resultado getResultado() {
+        return resultado;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public void setResultado(Resultado resultado) {
+        this.resultado = resultado;
+    }
+
+    public Partido(Seleccion seleccionLocal, Seleccion seleccionVisitante, String estadio, LocalDate fecha, LocalTime hora, Arbitro arbitro[]) {
+        this.seleccionLocal = seleccionLocal;
+        this.seleccionVisitante = seleccionVisitante;
+        this.estadio = estadio;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.arbitro = arbitro;
+        this.resultado = new Resultado();
+    }
     
-}
+   
+    
+    }
+    
+
